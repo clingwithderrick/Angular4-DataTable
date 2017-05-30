6bd6e5b1848df326b9231ab52bb391cabce9e82b
PERFICIENT\sugan.shunmugaraj <suganrmk@gmail.com>
2017-05-30 12:20:04 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DatatableComponent , CarService } from './datatable/datatable.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
+
+import {DataTableModule,SharedModule , DialogModule , ButtonModule , CalendarModule ,CheckboxModule ,
+        InputTextareaModule,  MultiSelectModule, DropdownModule, InputSwitchModule , RadioButtonModule , 
+        RatingModule} from 'primeng/primeng';
+import { FormComponent } from './form/form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatatableComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    DataTableModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    DialogModule,
+    ButtonModule,
+    Angular2FontAwesomeModule,
+    CalendarModule,
+    CheckboxModule,
+    DropdownModule,
+    InputSwitchModule,
+    InputTextareaModule,
+    MultiSelectModule,
+    RatingModule,
+    RadioButtonModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
